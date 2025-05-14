a31db39ddb19ebc5624bd513e6f080384ad599e0
Quentin LACOMBE <quentlace2g@gmail.com>
2025-05-14 08:23:45 +0200

    New v

--- src/pages/actions/core.tsx
+++ src/pages/actions/core.tsx
@@ -1,7 +1,6 @@
 "use server"
-
 export async function getTimeToOrder(metalQty: number, textileQty: number) : Promise<number> {
-    
-    return 25
+
+    return metalQty + textileQty;
 
 }
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,11 +1,31 @@
+"use client"
+import { useEffect, useState } from "react";
+import { getTimeToOrder } from "./actions/core";
 
-import { getTimeToOrder } from "./api/core";
+export default function Test() {
+  const [time, setTime] = useState<number>(0);
+  const [metalQty, setMetalQty] = useState<number>(0);
+  const [textileQty, setTextileQty] = useState<number>(0);
 
-export default async function Test() {
-  const qty = await getTimeToOrder(10, 10);
 
-  return (
-    <h1>{qty}</h1>
-   
+   useEffect(() => {
+    getTimeToOrder(metalQty, textileQty).then(q => setTime(q));
+   }, [metalQty, textileQty]);
+
+  return (<div className="h-full w-full">
+    <p className="text-2xl">{time}</p>
+
+    <span className="flex text-2xl align-center">
+      <p>Métal</p>
+      <input type="range" min={0} max="100" value={metalQty} className="range" onChange={e => setMetalQty(Number.parseInt(e.target.value))} />
+      <p>{metalQty} kg</p>
+    </span>
+
+    <span className="flex text-2xl align-center">
+      <p>Textile</p>
+      <input type="range" min={0} max="100" value={textileQty} className="range" onChange={e => setTextileQty(Number.parseInt(e.target.value))} />
+      <p>{textileQty} kg</p>
+    </span>
+    </div>
   );
 }
